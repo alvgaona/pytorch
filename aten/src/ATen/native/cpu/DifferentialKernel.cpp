47b46f1bdb849802ed5926e5379fcdff78728431
Alvaro <alvgaona@gmail.com>
2020-07-24 19:01:38 -0300

    feat: Add chebpoly_kernel

--- aten/src/ATen/native/cpu/DifferentialKernel.cpp
+++ aten/src/ATen/native/cpu/DifferentialKernel.cpp
@@ -1,23 +1,25 @@
-#include <ATen/native/BinaryOps.h>
-
-#include <cmath>
-#include <iostream>
-
 #include <ATen/Dispatch.h>
+#include <ATen/native/TensorIterator.h>
+
 #include <ATen/native/Differential.h>
+
 
 namespace at {
 namespace native {
+namespace {
 
-static void chebpoly_kernel(TensorIterator& iter, int order) {
-    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(iter.dtype(), "chebpoly_cpu", [&] {
+template <typename scalar_t>
+static void chebpoly_kernel(TensorIterator& iter, Scalar order) {
+  AT_DISPATCH_FLOATING_TYPES(iter.dtype(), "chebpoly_cpu", [&] {
+    auto n = order.to<scalar_t>();
     cpu_kernel(
         iter,
-        [=](scalar_t a) -> scalar_t {
-          return chebpoly_calc(a, order);
-        }
+        [=](scalar_t a) -> scalar_t { return chebpoly_calc(a, n); }
+    );
   });
 }
+
+} // namespace
 
 REGISTER_DISPATCH(chebpoly_stub, &chebpoly_kernel);
 
